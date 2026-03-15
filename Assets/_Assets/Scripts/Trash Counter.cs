6657fe4535c172c80fa9cc4f50bcfbae863b10e9
Tron-xR <tron.x.530@gmail.com>
2026-03-15 23:51:18 +0530

    y

--- Assets/_Assets/Scripts/Trash Counter.cs
+++ Assets/_Assets/Scripts/Trash Counter.cs
@@ -4,6 +4,7 @@ public class TrashCounter : BaseCounter
 {
     public override void Interact(Player player)
     {
+        //trash counter
         if (player.HasKitchenObject())
         {
             player.GetKitchenObject().DestroySelf();
